a7b379802055c870d1c6718b633e2292a74e55ef
Hassan-jr <abdiladifhassan115@gmail.com>
2022-06-17 13:43:42 +0300

    gitignore change

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express()
 app.use (express.json())
 app.use(cors())
 app.use (express.urlencoded({extended: false}))
-//const port = process.env.PORT || 4000
+const port =   process.env.PORT || 4000
 
 
 app.use('/', require('./Routes/homeroute') )
