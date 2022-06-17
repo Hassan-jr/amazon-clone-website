b9ca44bcdafdc07e6a5b6c001532200aaa2d5dea
Hassan-jr <abdiladifhassan115@gmail.com>
2022-06-17 11:09:45 +0300

    main change

--- backend/server.js
+++ backend/server.js
@@ -31,5 +31,5 @@ if (process.env.NODE_ENV === 'production') {
 
 
 app.listen (process.env.PORT || 80 , ()=>{
-    console.log(`listening on port ${process.env.PORT} `);
+    console.log('listening on port');
 })
