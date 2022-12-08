4c74ebf516772aaf3faec152b5f3f72d02abab3d
Hassan-jr <abdiladifhassan115@gmail.com>
2022-12-08 11:43:54 +0300

    build folder added

---
+++ frontend/build/precache-manifest.ec07fcdf7fe68b419e16b884d5cdc7ef.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "05b4d1b6656dd5b42e9b",
+    "url": "/static/css/main.031b34f0.chunk.css"
+  },
+  {
+    "revision": "05b4d1b6656dd5b42e9b",
+    "url": "/static/js/main.05b4d1b6.chunk.js"
+  },
+  {
+    "revision": "2f5fd013ba6c8847c915",
+    "url": "/static/css/1.6690fd4b.chunk.css"
+  },
+  {
+    "revision": "2f5fd013ba6c8847c915",
+    "url": "/static/js/1.2f5fd013.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "df2bda13705add66718991fb293fbbeb",
+    "url": "/static/media/header1.df2bda13.jpg"
+  },
+  {
+    "revision": "5a8340d4d7382fa9ce375f074383eaf3",
+    "url": "/static/media/header2.5a8340d4.jpg"
+  },
+  {
+    "revision": "41b82b30dab30a616d1842f49b926b42",
+    "url": "/static/media/header3.41b82b30.jpg"
+  },
+  {
+    "revision": "01c3525fa36e4ea4464e6c2fee84424a",
+    "url": "/static/media/header4.01c3525f.jpg"
+  },
+  {
+    "revision": "2942f2f3f377d3e83aa789cbec45bbaa",
+    "url": "/index.html"
+  }
+];
